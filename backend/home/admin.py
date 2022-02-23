90396f6c1d7213196b76cbcbfc5828743a97fd18
Crowdbotics <engineering@crowdbotics.com>
2022-02-23 18:12:46 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Testingsaap
+
+admin.site.register(Testingsaap)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class Testingsaap(models.Model):
+    "Generated Model"
+    testedapp = models.TimeField()
+    checktest = models.SlugField(
+        max_length=50,
+    )
